b5dcbc5100ff46619f53882ce228dbefd6a01a55
Jarrod <jarrod.miller@ttu.edu>
2019-03-27 14:41:31 -0500

    Added basic server functionality

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));

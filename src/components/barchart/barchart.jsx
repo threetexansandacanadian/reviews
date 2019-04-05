e34e638d0a3203c9dbd19895f1624de9200903f1
Jarrod <jarrod.miller@ttu.edu>
2019-04-04 19:11:15 -0500

    Added bargraph size control and added a nice little review average summary.

--- src/components/barchart/barchart.jsx
+++ src/components/barchart/barchart.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactStars from 'react-stars';
-import { VictoryBar } from 'victory';
+import { VictoryBar } from 'victory-bar';
+import { VictoryChart } from 'victory-chart';
+import { VictoryAxis } from 'victory-axis';
+import { Main, Chart } from './barchartStyles';
 
 export default function BarChart(props) {
   const countRatings = () => {
-    const counter = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+    const counter = { 5: 0, 4: 0, 3: 0, 2: 0, 1: 0 };
     props.reviews.forEach((review) => {
       counter[review.stars] += 1;
     });
@@ -13,10 +16,43 @@ export default function BarChart(props) {
     return results;
   };
 
+  const calcAverageRatings = () => {
+    let ratings = countRatings();
+    let sum = 0;
+    
+    Object.keys(ratings).forEach((rating) => {
+      console.log(rating, ratings[rating]);
+      sum += parseInt(ratings[rating].x) * ratings[rating].y;
+    });
+    let averageRating = (sum / props.reviews.length).toFixed(1);
+    //averageRating = Math.ceil(averageRating * 2) / 2;
+    return averageRating;
+  };
+
   return (
-    <div>
-      <VictoryBar data={countRatings()} />
-    </div>
+    <Main>
+      <h3>{props.reviews.length} reviews</h3>
+      <ReactStars
+        value={calcAverageRatings()}
+        edit={false}
+      />
+      <p>{calcAverageRatings()} out of 5 stars</p>
+      <div style={{backgroundColor: "#fee", height: '20vh', width: '50vw' }}>
+        <VictoryChart
+          height={300}
+        >
+          <VictoryAxis
+            tickValues={[5, 4, 3, 2, 1]}
+            tickFormat={x => `${x} stars`}
+          />
+          <VictoryBar
+            data={countRatings()}
+            labels={data => (`${Math.floor((data.y / props.reviews.length) * 100)}%`)}
+            horizontal
+          />
+        </VictoryChart>
+      </div>
+    </Main>
   );
 }
 
